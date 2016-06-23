ae2d07ef6e958716fa82f04e6d8faaf1a64b2349
Joseph <josephbates25@gmail.com>
2016-06-23 16:10:08 -0500

    init

--- store/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
-
-def index(request):
-    user = authenticate(username='joseph', password='cabrini93')
-    return HttpResponse("Hello World")
